058b918befc1c8254f2cbce6525e0d9d449e9be2
dariomiceli3 <dario.miceli@mail.polimi.it>
2020-06-24 13:19:32 +0200

    try to dinamically create card

--- public/assets/js/crew.js
+++ public/assets/js/crew.js
@@ -16,8 +16,16 @@ async function getPerson() {
             }
             return response.json();
         }).then(function (json) {
+            for (let s of json){
+                let n = s.name
+                let su = s.surname
+                let img = s.img
+                let id = s.role
+                $('#people-row').append(create(img, n, su, id))
 
-            console.log(json);
+            }
+
+            //console.log(json);
         });
 
     }
@@ -27,3 +35,24 @@ async function getPerson() {
     }
 
 }
+  function create(img, n, su, id) {
+      return '<div class="col-lg-4 mb-4">'+
+          '<div class="card h-100 text-center">'+
+          '<img class="card-img-top" src='+ img + 'alt="Missing">' +
+          '<div class="card-body">' +
+          '<h4 class="card-title">'+ n + ' '+ su + '</h4>' +
+          '<p class="card-text">' + id + '</p>' +
+          '<button type="button" class="btn btn-link">SEE MORE</button>' +
+          '</div>' +
+          '</div>' +
+          '</div>'
+
+  }
+//function append_person(img, name, surname, id){
+    //return $('<div class="col-lg-4 mb-4">  <div class="card h-100 text-center">').append(
+      //  $('<img class="card-img-top" alt="Missing" src="' + img + '">'),
+        //$(` <div class="card-body">`).append(
+          //  $(' <h4 class="card-title">' +name+' </h4>'), $(' </div> </div> </div>')
+       // )
+    //);
+//}
