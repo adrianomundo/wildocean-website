9b5bbd4e0c815e758feab7bccc8c3a6d6990ec91
dariomiceli3 <dario.miceli@mail.polimi.it>
2020-06-25 10:33:06 +0200

    fix json

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -9,14 +9,28 @@ $(document).ready( function() {
 
 async function getEvents() {
     try {
-        fetch("https://wildocean.herokuapp.com/api/v1/events/2/service").then(function (response) {
+        fetch("https://wildocean.herokuapp.com/api/v1/events").then(function (response) {
             if (!response.ok) {
                 console.log("HTTPS API Error, status = " + response.status);
             }
             return response.json();
         }).then(function (json) {
+            let html = '<div class="row"> '
+            for (let s of json) {
+                let t = s.title
+                let d = s.date
+                let img = s.img
+                let st = s.start_h
+                let e = s.end_t
+                let l = s.location
+                //console.log(img);
+                html += create(img, t, d, st, e, l)
+                //console.log(img_circle);
 
-            console.log(json);
+            }
+            html += '</div>'
+            $('#all-events').append(html)
+            //console.log(json);
         });
 
     }
@@ -24,5 +38,21 @@ async function getEvents() {
         location.replace("./404.html");
         console.log(e);
     }
+
+}
+
+function create(img, t, d, st, e, l) {
+    return '<div class="col-lg-4 mb-4">'+
+        '<div class="card h-100 text-center" style="border-radius: 30px>"> ' +
+        '<img class="card-img-top" alt="Missing" src='+ img + ' '+ '>' +
+        '<div class="card-body">' +
+        '<h4 class="card-title">'+ t + '</h4>' +
+        '<p class="card-text">' + d + '</p>' +
+        '<p class="card-text">' + st + ' ' + e + '</p>' +
+        '<p class="card-text">' + l + '</p>' +
+        '<button type="button" class="btn btn-link" >FIND OUT MORE</button>' +
+        '</div>' +
+        '</div>' +
+        '</div>'
 
 }
