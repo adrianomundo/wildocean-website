108de919a255869ac20616309764ff8932f1ae16
Adriano Mundo <adrianomundo23@gmail.com>
2020-06-25 15:24:04 +0200

    dynamic footer loading

---
+++ public/assets/js/landmarks.js
@@ -0,0 +1,50 @@
+"use strict";
+
+//$("#navbar").load("../pages/components/navbar.html");
+
+$(document).ready( () => {
+
+    loadFooter();
+
+});
+
+function loadFooter() {
+
+    let html = `<footer class="py-5 nopadding">
+    <div class="container-full" >
+        <div class="row">
+            <div class="col md-6">
+                <img src="./public/assets/img/logo.png" alt="Logo"  width="110" height="100">
+            </div>
+            <div class="col md-6">
+                <p><span style="font-size: 13px">Via Edoardo Sagrada,46</span></p>
+                <p><span style="font-size: 13px">Milano MI</span></p>
+                <p><span style="font-size: 13px">+39.2.2399.3617</span></p>
+                <p><span style="font-size: 13px">info@wildocean.com</span></p>
+                <div  class="fa fa-instagram" ></div>
+                <div  class="fa fa-facebook"></div>
+                <div  class="fa fa-twitter"></div>
+                <div  class="fa fa-linkedin"></div>
+                <div  class="fa fa-youtube"></div>
+            </div>
+            <div class="col sm-6">
+                <p style="padding: 10px;"><a class="footer-link" href="./public/pages/about.html"><b>About</b></a></p>
+                <p style="padding: 10px;"><a class="footer-link" href="./public/pages/services.html"><b>Services</b></a></p>
+                <p style="padding: 10px;"><a class="footer-link" href="./public/pages/crew.html"><b>Crew</b></a></p>
+            </div>
+            <div class="col sm-6">
+                <p style="padding: 10px;"><a class="footer-link" href="./public/pages/events.html"><b>Events</b></a></p>
+                <p style="padding: 10px;"><a class="footer-link" href="./public/pages/contact.html"><b>Contact</b></a></p>
+            </div>
+            <div class="col sm-6">
+                <p style="padding: 5px;">Privacy Policy</p>
+                <p style="padding: 5px;">Cookie Policy</p>
+                <p style="padding: 5px;">Terms & Conditions</p>
+            </div>
+        </div>
+        <p class="m-0 text-center text-black"> &copy;<span style="font-size: 13px"> Wild Ocean | 21/04/2013</span></p>
+    </div>
+</footer>`;
+
+    $("#footer").append(html);
+}
