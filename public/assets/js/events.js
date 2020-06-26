261c1b7489b432867acd09451da70775e98c8755
dariomiceli3 <dario.miceli@mail.polimi.it>
2020-06-26 12:22:29 +0200

    kot_event.js

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -24,8 +24,9 @@ async function getEvents() {
                 let st = s.start_h
                 let e = s.end_h
                 let l = s.location
+                let event_id = s.event_id
                 //console.log(img);
-                html += create(img, t, di, st, e, l)
+                html += create(img, t, di, st, e, l, event_id)
                 //console.log(img_circle);
 
             }
@@ -42,7 +43,7 @@ async function getEvents() {
 
 }
 
-function create(img, t, d, st, e, l) {
+function create(img, t, d, st, e, l, event_id) {
     return '<div class="col-lg-4 mb-4">'+
         '<div class="card h-100 text-center" style="border-radius: 30px>"> ' +
         '<img class="card-img-top" alt="Missing" src='+ img + ' '+ '>' +
@@ -51,7 +52,7 @@ function create(img, t, d, st, e, l) {
         '<p class="card-text">' + d + '</p>' +
         '<p class="card-text">' + st + ' - ' + e + '</p>' +
         '<p class="card-text">' + l + '</p>' +
-        '<button type="button" class="btn btn-link" >FIND OUT MORE</button>' +
+        '<a href="kot_event.html?id=' + event_id +'" class="btn btn-outline-primary" role="button"><span style="font-size: 14px"><b>FIND OUT MORE</b></span></a>' +
         '</div>' +
         '</div>' +
         '</div>'
