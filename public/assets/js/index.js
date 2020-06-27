0efdb7f0fdb3aba9ce5308d2ccc41dfc31c278d9
Adriano Mundo <adrianomundo23@gmail.com>
2020-06-27 19:23:54 +0200

    home and service html + js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,95 +3,114 @@
 $(document).ready( function() {
 
     //fetchServices();
-    fetchTestimonials()
+    fetchHomepage();
 });
 
-async function fetchTestimonials() {
-    let response = (await fetch("https://wildocean.herokuapp.com/api/v1/testimonials"));
-    let testimonials = await response.json();
-    console.log(testimonials);
-    let html = '<div class="container-fluid nopadding" >' +
-        '          <div id="carousel_testimonial" class="carousel slide" data-ride="carousel">'
+async function fetchHomepage() {
 
-        html +=  '<div class="carousel-inner" role="listbox">'
-
-        for ( let i = 0; i < testimonials.length; i++ ) {
-          console.log(testimonials[i])
-            if (i == 0) html += createSlideFirst(testimonials[i]);
-            else html += createSlide(testimonials[i]);
-        }
-
-        html +=
-
-        '          <a class="carousel-control-prev" href="carousel_testimonial" role="button" data-slide="prev">' +
-        '              <span class="carousel-control-prev-icon" aria-hidden="true"></span>' +
-        '              <span class="sr-only">Previous</span>' +
-        '          </a>' +
-        '          <a class="carousel-control-next" href="#carousel_testimonial" role="button" data-slide="next">' +
-        '              <span class="carousel-control-next-icon" aria-hidden="true"></span>' +
-        '              <span class="sr-only">Next</span>' +
-        '          </a>' +
-        '          </div>'
-
-    //for ( let i = 0; i < testimonials.length; i++ ) {
-      //  console.log(testimonials[i])
-      //  html += createSlide(testimonials[i]);
-   // }
-
-    //html += '</div>' +
-       /* '  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">' +
-        '    <span class="carousel-control-prev-icon" aria-hidden="true"></span>' +
-        '    <span class="sr-only">Previous</span>' +
-        '  </a>' +
-        '  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">' +
-        '    <span class="carousel-control-next-icon" aria-hidden="true"></span>' +
-        '    <span class="sr-only">Next</span>' +
-        '  </a>' +*/
-       // '</div>'
-
-    $('#testimonial').append(html)
-}
-
-async function fetchServices() {
-    try {
-        fetch("https://wildocean.herokuapp.com/api/v1/services").then(function (response) {
-            if (!response.ok) {
-                console.log("HTTPS API Error, status = " + response.status)
-            }
-            return response.json();
-        }).then(function (json) {
-            let service_1 = json[0];
-            let service_2 = json[1];
-            displayServices(service_1.service_id, service_1.title, service_1.short_description);
-            displayServices(service_2.service_id, service_2.title, service_2.short_description);
-            //TODO aggiungere la prima immagine
-            //TODO link alla pagine del singolo servizio
-        })
+    let service_response = (await fetch("https://wildocean.herokuapp.com/api/v1/services"));
+    if (!service_response.ok) {
+        console.log("HTTPS API Error, status = " + service_response.status);
+        location.replace("../assets/pages/404.html");
     }
-    catch (e) {
-        location.replace("./404.html");
-        console.log(e);
+    let service = await service_response.json();
+    $("#text").text(service[0].short_description);
+    $("#text2").text(service[0].short_description);
+
+    let testimonials_response = (await fetch("https://wildocean.herokuapp.com/api/v1/testimonials"));
+    if (!testimonials_response.ok) {
+        console.log("HTTPS API Error, status = " + testimonials_response.status);
+        location.replace("../assets/pages/404.html");
     }
+    let testimonials = await testimonials_response.json();
+
+    let html = "";
+    html += displayTestimonialSlider(testimonials);
+
+    let html_2 = displayServices(service);
+
+    $("#testimonial").append(html);
+    $("#services").append(html_2);
 }
 
+function displayTestimonialSlider(testimonials) {
 
-function displayServices(service_id, title, description) {
-    $("#title" + service_id).text(title);
-    $("#description" + service_id).text(description);
+    let html = '<div class="container-fluid nopadding">' +
+        '<div id="carousel_testimonial" class="carousel slide" data-ride="carousel">';
+
+    html += '<div class="carousel-inner" role="listbox">' +
+        '<div class="carousel-item active text-center"  style="height: 300px; padding-top: 55px"> '+
+        '<div class="container">' +
+            '<h3>'+ testimonials[0].review + '</h3>' +
+        '</div>' +
+        '<h4>' + testimonials[0].name + ' ' + testimonials[0].surname + '</h4>' + '</div>';
+
+    for (let i = 1; i < testimonials.length; i++) {
+        html += '<div class="carousel-item text-center"  style="height: 300px; padding-top: 55px"> '+
+            '<div class="container"> <h3>'+ testimonials[i].review + '</h3>' + '</div>'  +
+            '<h4>' + testimonials[i].name + ' ' + testimonials[i].surname + '</h4>' +
+            '</div>';
+    }
+
+    html += '<a class="carousel-control-prev" href="#carousel_testimonial" role="button" data-slide="prev">' +
+        '<span class="carousel-control-prev-icon" aria-hidden="true"></span>' +
+        '<span class="sr-only">Previous</span>' +
+        '</a>' +
+        '<a class="carousel-control-next" href="#carousel_testimonial" role="button" data-slide="next">' +
+        '<span class="carousel-control-next-icon" aria-hidden="true"></span>' +
+        '<span class="sr-only">Next</span>' +
+        '</a>' + '</div>';
+
+    return html;
+
 }
 
-function createSlide(testimonial) {
-    return '<div class="carousel-item  text-center"  style="height: 300px; padding-top: 100px"> '+
-        '      <div class="container"> <h3>'+ testimonial.review + '</h3> </div>'  +
-        //'        <div class="row text-center">' + testimonial.name + ' ' + testimonial.surname + '</div>'  +
-        '       <h4>' + testimonial.name + ' ' + testimonial.surname + '</h4>' +
-        '    </div>';
+function displayServices(service) {
+
+    let service_one = service[Math.floor(Math.random() * service.length)];
+    console.log(service_one);
+
+    const index = service.indexOf(service_one);
+    service.splice(index, 1);
+
+    let service_two = service[Math.floor(Math.random() * service.length)];
+
+
+    return '<div class="container nopadding" style="text-align: left">' +
+              '<div class="row">' +
+                  '<div class="col-md-5">' +
+                      '<img class="img-fluid" alt="img_service_2" src="'+service_one.img[0].slice(3)+'">' +
+                  '</div>' +
+                  '<div class="col-md-7" style="margin-top: 20px;">' +
+                     '<div class="item">' +
+                          '<h3 style="color: #0077C0">'+service_one.title+'</h3>' +
+                          '<p id="text" style="margin-top: 15px">'+service_one.short_description+'</p>' +
+                      '</div>' +
+                      '<div class="text-left" style="margin-left: 20px">' +
+                         '<a href="./pages/service.html?id='+service_one.service_id+'" class="btn btn-outline-primary" role="button"><span style="font-size: 14px"><b>FIND OUT MORE</b></span></a>' +
+                      '</div>' +
+                  '</div>' +
+              '</div>' +
+          '</div>' +
+          '<div class="container nopadding">' +
+              '<hr style="width: 90%; height: 1px; color: #4eb5e5; background-color: #4eb5e5; margin-top: 80px; margin-bottom: 80px">' +
+          '</div>' +
+                  '<div class="container nopadding">' +
+              '<div class="row">' +
+                  '<div class="col-md-7" style="margin-top: 20px; text-align: left" >' +
+                      '<div class="item">' +
+                          '<h3 style="color: #0077C0">'+ service_two.title +'</h3>' +
+                          '<p id="text2" style="margin-top: 15px">'+ service_two.short_description +'</p>' +
+                      '</div>' +
+                      '<div class="text-left" style="margin-left: 20px; margin-bottom: 70px">' +
+                          '<a href="./pages/service.html?id='+ service_two.service_id +'" class="btn btn-outline-primary" role="button"><span style="font-size: 14px;"><b>FIND OUT MORE</b></span></a>' +
+                      '</div>' +
+                  '</div>' +
+                  '<div class="col-md-5">' +
+                      '<img src="'+service_two.img[0].slice(3)+'" class="img-fluid" alt="img_service_2">' +
+                  '</div>' +
+              '</div>' +
+          '</div>';
+
 }
 
-function createSlideFirst(testimonial) {
-    return '<div class="carousel-item active text-center"  style="height: 300px; padding-top: 100px"> '+
-        '      <div class="container"> <h3>'+ testimonial.review + '</h3> </div>'  +
-        //'          <div class="row text-center">' + testimonial.name + ' ' + testimonial.surname + '</div>'  +
-        '        <h4>' + testimonial.name + ' ' + testimonial.surname + '</h4>' +
-        '    </div>';
-}
--- public/assets/js/service.js
+++ public/assets/js/service.js
@@ -11,38 +11,47 @@ async function fetchService() {
     let service_id = serviceToDisplay();
 
     let service_response = (await fetch("https://wildocean.herokuapp.com/api/v1/services/" + service_id));
-        if (!service_response.ok) {
-            console.log("HTTPS API Error, status = " + service_response.status);
-            location.replace("../assets/pages/404.html");
-        }
+    if (!service_response.ok) {
+        console.log("HTTPS API Error, status = " + service_response.status);
+        location.replace("../assets/pages/404.html");
+    }
     let service = await service_response.json();
 
     let event_response = (await fetch("https://wildocean.herokuapp.com/api/v1/services/" + service_id + "/event"));
-        if (!event_response.ok) {
-            console.log("HTTPS API Error, status = " + event_response.status);
-            location.replace("../assets/pages/404.html");
-        }
+    if (!event_response.ok) {
+        console.log("HTTPS API Error, status = " + event_response.status);
+        location.replace("../assets/pages/404.html");
+    }
     let event = await event_response.json();
 
     let people_response = (await fetch("https://wildocean.herokuapp.com/api/v1/services/" + service_id + "/people"));
-        if (!people_response.ok) {
-            console.log("HTTPS API Error, status = " + people_response.status);
-            location.replace("../assets/pages/404.html");
-        }
+    if (!people_response.ok) {
+        console.log("HTTPS API Error, status = " + people_response.status);
+        location.replace("../assets/pages/404.html");
+    }
     let people = await people_response.json();
 
-    console.log(service[0]);
+    console.log(service[0].img);
     console.log(event[0]);
     console.log(people);
+    console.log(people[0]);
 
     pageTitle(service[0]);
 
     let html = "";
-    html = displayServiceSlider(service[0]);
-    html += displayServiceInfo(service[0]);
-    html += displayServiceEvent(service[0]);
-    html += displayServiceCrew(people[0]);
 
+    let imgArray = service[0].img;
+    html += displayServiceSlider(imgArray);
+    html += displayOrientation(service[0]);
+    html += displayServiceInfo(service[0]);
+    if (event.length > 0) {
+        html += displayServiceEvent(event[0]);
+    }
+    html += createCrewContainer();
+    for (let i = 0; i < people.length; i++) {
+        let img_circle = personRounded(people[i].img)
+        html += displayServiceCrew(people[i], img_circle);
+    }
     $("#service").append(html);
 
 }
@@ -62,38 +71,46 @@ function pageTitle(service) {
     $("#page_title").text("Wild Ocean | " + service.title);
 }
 
-// slider + orientation info
-function displayServiceSlider(service) {
-    return `<div class="container-fluid nopadding">
-          <div id="carousel_service" class="carousel slide" data-ride="carousel">
-              <ol class="carousel-indicators">
-                  <li data-target="#carousel_service" data-slide-to="0" class="active"></li>
-                  <li data-target="#carousel_service" data-slide-to="1"></li>
-              </ol>
-          <div class="carousel-inner" role="listbox">
-              <div class="carousel-item active">
-                  <img class="img-slider" src="`+ service.img[0] + `" alt="service_first_img">
-              </div>
-              <div class="carousel-item">
-                  <img class="img-slider" src="`+ service.img[1] + `" alt="service_second_img">
-              </div>
-          </div>
-          <a class="carousel-control-prev" href="#carousel_service" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carousel_service" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-          </a>
-          </div>
-          <nav aria-label="breadcrumb">
+function displayServiceSlider(imgArray) {
+
+    let html = '<div class="container-fluid nopadding">' +
+        '<div id="carousel_service" class="carousel slide" data-ride="carousel">' +
+        '<ol class="carousel-indicators">' +
+        '<li data-target="#carousel_service" data-slide-to="0" class="active"></li>';
+    for (let i = 1; i < imgArray.length; i++) {
+        html += '<li data-target="#carousel_service" data-slide-to=" ' + i + '"></li>';
+    }
+    html += '</ol>';
+
+    html += '<div class="carousel-inner" role="listbox">' +
+        '<div class="carousel-item active">' +
+        '<img class="img-fluid" src=" ' + imgArray[0] + '" alt="service_first_img">' +
+        '</div>';
+    for (let i = 1; i < imgArray.length; i++) {
+        html += '<div class="carousel-item">' +
+            '<img class="img-fluid" src="' + imgArray[i] + '" alt="service_second_img">' +
+            '</div>' +
+            '</div>';
+    }
+    html += '<a class="carousel-control-prev" href="#carousel_service" role="button" data-slide="prev">' +
+        '<span class="carousel-control-prev-icon" aria-hidden="true"></span>' +
+        '<span class="sr-only">Previous</span>' +
+        '</a>' +
+        '<a class="carousel-control-next" href="#carousel_service" role="button" data-slide="next">' +
+        '<span class="carousel-control-next-icon" aria-hidden="true"></span>' +
+        '<span class="sr-only">Next</span>' +
+        '</a>' + '</div>';
+    return html;
+}
+
+function displayOrientation(service) {
+    return `<nav aria-label="breadcrumb">
               <ol class="breadcrumb">
                   <li class="breadcrumb-item">
                       <a href="services.html" aria-current="page" style="color: black">Services</a>
                   </li>
                   <li class="breadcrumb-item active">
-                      <a id="breadcrumb_service_title" aria-current="page" style="color: #0077C0">`+ service.title +`</a>
+                      <a id="breadcrumb_service_title" aria-current="page" style="color: #0077C0"> `+ service.title +`</a>
                   </li>
               </ol>
           </nav>
@@ -103,9 +120,9 @@ function displayServiceSlider(service) {
 // service and practical info
 function displayServiceInfo(service) {
     return `<div class="container" id="service_info" style="text-align: center">
-          <h1 class="custom_heading align-center" style="text-align: center"><b>`+ service.title +`</b></h1>
+          <h1 class="custom_heading align-center"><b>`+ service.title +`</b></h1>
           <p>`+ service.long_description +`</p>
-          <h2 class="custom_heading align-center" style="text-align: center"><b>Practical Info</b></h2>
+          <h2 class="custom_heading align-center"><b>Practical Info</b></h2>
           <p>`+ service.practical_info +`</p>
       </div>`;
 }
@@ -117,14 +134,13 @@ function displayServiceEvent(event) {
           <h2 class="custom_heading align-center"><strong>Featured Event</strong></h2>
           <div class="row" id="featured_event">
               <div class="col-lg-5 text-center">
-                  <img class="img-fluid rounded" src="" alt="ocean_talk">
+                  <img class="img-fluid rounded" src="`+ event.img +` " alt="ocean_talk">
               </div>
               <div class="col-lg-7 text-left" style="margin-top: 20px;">
-                <h3 style="color: #0077C0; margin-bottom: 20px;"><strong>Event Title</strong></h3>
-                <p>Each autumn we brace ourselves against the inevitable mass accumulation of plastic pollution upon our beaches.
-                    Each autumn we brace ourselves against the inevitable mass accumulation of plastic pollution upon our beaches.</p>
+                <h3 style="color: #0077C0; margin-bottom: 20px;">`+ event.title +`</h3>
+                <p>`+ event.short_description +`</p>
                 <div class="text-left" style="margin-top: 30px;">
-                    <a href="service.html?id=\`+event_id+\`" class="btn btn-outline-primary" role="button"><span style="font-size: 14px"><b>FIND OUT MORE</b></span></a>
+                    <a href="event.html?id= `+ event.event_id +`" class="btn btn-outline-primary" role="button"><span style="font-size: 14px"><b>FIND OUT MORE</b></span></a>
                 </div>
               </div>
           </div>
@@ -132,27 +148,27 @@ function displayServiceEvent(event) {
 
 }
 
-// related crew
-function displayServiceCrew(people) {
-
+function createCrewContainer() {
     return `<div class="container" id="service_crew" style="text-align: center">
-          <h2 class="custom_heading align-center" id="crew" style="text-align: center"><strong>Related Crew</strong></h2>
-          <div class="row justify-content-center">
-              <div class="col-lg-4 mb-4">
-                  <div class="card h-100 text-center" style="border-radius: 15px">
-                    <img class="card-img-top" alt="Missing" src="../assets/img/person/Person3_rounded.svg" height="250" style="padding-top: 20px">
-                  <div class="card-body">
-                      <h4 class="card-title">Adriano Mundo</h4>
-                      <p class="card-text">Chief Innovation Officer</p>
-                  <div class="text-center" style="margin-top: 20px;">
-                      <a href="service.html?id=\`+person_id+\`" class="btn btn-outline-primary" role="button"><span style="font-size: 14px"><b>FIND OUT MORE</b></span></a>
-                  </div>
-                  </div>
-                  </div>
-              </div>
-          </div>
-      </div>`
+        <h2 class="custom_heading align-center" id="crew" style="text-align: center"><strong>Related Crew</strong></h2>
+    <div class="row justify-content-center" id="crew_row">`;
+}
 
+// related crew
+function displayServiceCrew(people, img_circle) {
+
+    return `<div class="col-lg-4 mb-4">
+                  <div class="card h-100 text-center" style="border-radius: 15px">
+                    <img class="card-img-top" alt="Missing" src="`+ img_circle +`" height="250" style="padding-top: 20px">
+                  <div class="card-body">
+                      <h4 class="card-title">`+ people.name +` ` + people.surname +`</h4>
+                      <p class="card-text">`+ people.role + `</p>
+                  <div class="text-center" style="margin-top: 20px;">
+                      <a href="person.html?id=` + people.matricola + `" class="btn btn-outline-primary" role="button"><span style="font-size: 14px"><b>FIND OUT MORE</b></span></a>
+                  </div>
+                  </div>
+                  </div>
+              </div>`;
 }
 
 // utils
