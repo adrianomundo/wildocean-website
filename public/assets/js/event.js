7badaa46c105273cda8c260e9ebbcd85cfcaecf1
Adriano Mundo <adrianomundo23@gmail.com>
2020-06-29 16:09:29 +0200

    event page

--- public/assets/js/event.js
+++ public/assets/js/event.js
@@ -41,19 +41,23 @@ async function fetchEvent() {
     let html = displayOrientation(event[0]);
     html += displayEvent(event[0]);
 
-    html += createRelationContainer();
-    html += displayEventPeople(person[0]);
+    let img_circle = personRounded(person[0].img);
 
     if (service.length > 0) {
-        let service_response = (await fetch("https://wildocean.herokuapp.com/api/v1/services/"+ service[0].service_id));
-        if (!service_response.ok) {
-            console.log("HTTPS API Error, status = " + service_response.status);
+        let service_response_new = (await fetch("https://wildocean.herokuapp.com/api/v1/services/" + service[0].service_id));
+        if (!service_response_new.ok) {
+            console.log("HTTPS API Error, status = " + service_response_new.status);
             location.replace("../assets/pages/404.html");
         }
-        let service_new = await service_response.json();
-        console.log(service);
-        html += displayEventService(service_new[0]);
+        let service_new = await service_response_new.json();
+
+        let img_circle_2 = serviceRounded(service_new[0].img[0]);
+        html += displayEventAndService(person[0], service_new[0], img_circle, img_circle_2);
     }
+    else {
+        html += displayEventPeople(person[0], img_circle);
+    }
+
 
     //html += displayNavigationItems(event[0]);
 
@@ -87,72 +91,92 @@ function displayOrientation(event) {
         '    </ol>';
 }
 
+function displayTitle() {
+
+}
+
 function displayEvent(event){
-    return   '<div class="row justify-content-center">' +
-        '<div class="col-lg-6">' +
-        '<img class="img-fluid rounded" alt="Event_img" src=' + event.img + '>' +
-        '</div>' +
-        '<div class="col-lg-6 text-left">'+
-        '<h2 style="margin-bottom: 25px; margin-left: 10px; margin-top: 10px">' + event.title +'</h2>' +
-        '<div class="date">' +'' +
-        '<i class="fa fa-calendar" style="font-size: 20px;"></i>' +
-        '<p style="display: inline-block">' + correctDate(event.date) + '</p>' +
-        '</div>' +
-        '<div class="hour">' +'' +
-        '<i class="fa fa-clock-o" style="font-size: 20px;"></i>' +
-        '<p style="display: inline-block">' + event.start_h + ' - ' + event.end_h+ '</p>' +
-        '</div>' +
-        '<div class="nation">' +
-        '<i class="fa fa-globe" style="font-size: 20px;"></i>' +
-        '<p style="display: inline-block">' + event.location + '</p>' +
-        '</div>'+
-        '<div class="phone">' +'' +
-        '<i class="fa fa-mobile" style="font-size: 20px;"></i>' +
-        '<p style="display: inline-block">' + event.phone + '</p>' +
-        '</div>'+
-        '<div  class="fa fa-instagram" ></div>\n' +
-       '<div  class="fa fa-facebook"></div>\n' +
-        '<div  class="fa fa-twitter"></div>\n' +
-        '<div  class="fa fa-linkedin"></div>\n' +
-        '<div  class="fa fa-youtube"></div>\n' +
-        '</div>' +
-        '<div class=container style="margin-top: 30px; text-align: left; justify-content: left">' +
-        '<p>' + event.long_description + '</p>' +
+    return  '<div class="row justify-content-center">' +
+                '<div class="col-lg-6">' +
+                    '<img class="img-fluid rounded" alt="Event_img" src=' + event.img + '>' +
+                '</div>' +
+                '<div class="col-lg-6 text-left">'+
+                    '<h2 style="margin-bottom: 25px; margin-left: 10px; margin-top: 10px">' + event.title +'</h2>' +
+                    '<div class="date">' +'' +
+                        '<i class="fa fa-calendar" style="font-size: 20px;"></i>' +
+                        '<p style="display: inline-block">' + correctDate(event.date) + '</p>' +
+                    '</div>' +
+                    '<div class="hour">' +'' +
+                        '<i class="fa fa-clock-o" style="font-size: 20px;"></i>' +
+                        '<p style="display: inline-block">' + event.start_h + ' - ' + event.end_h+ '</p>' +
+                    '</div>' +
+                    '<div class="nation">' +
+                        '<i class="fa fa-globe" style="font-size: 20px;"></i>' +
+                        '<p style="display: inline-block">' + event.location + '</p>' +
+                    '</div>'+
+                    '<div class="phone">' +'' +
+                        '<i class="fa fa-mobile" style="font-size: 20px;"></i>' +
+                        '<p style="display: inline-block">' + event.phone + '</p>' +
+                    '</div>'+
+                    '<div  class="fa fa-instagram" ></div>' +
+                    '<div  class="fa fa-facebook"></div>' +
+                    '<div  class="fa fa-twitter"></div>' +
+                    '<div  class="fa fa-linkedin"></div>' +
+                    '<div  class="fa fa-youtube"></div>' +
+                '</div>' +
+            '</div>' +
+
+            '<div class=container style="margin-top: 30px; text-align: left; justify-content: left">' +
+                 '<p>' + event.long_description + '</p>' +
+            '</div>';
+}
+
+function displayEventAndService(person, service, img_circle, img_circle_2) {
+
+    return '<div class="container" style="text-align: center">' +
+        '<div class="row justify-content-center">' +
+                '<div class="col-lg-4 mb-4">'+
+                    '<h2 class="custom_heading align-center">Organiser</h2>' +
+                    '<div class="card text-center" style="border-radius: 15px">' +
+                        '<img class="card-img-top" alt="person_img" src="'+ img_circle +'" height="250" style="padding-top: 20px">' +
+                        '<div class="card-body">' +
+                            '<h4 class="card-title">'+ person.name + ' '+ person.surname + '</h4>' +
+                            '<p class="card-text">' + person.role + '</p>' +
+                            '<a href="person.html?id='+ person.matricola +'" class="btn btn-outline-primary" role="button"><span style="font-size: 14px"><b>SEE MORE</b></span></a>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>' +
+                '<div class="col-lg-4 mb-4">'+
+                    '<h2 class="custom_heading align-center">Service</h2>' +
+                    '<div class="card text-center" style="border-radius: 15px>">' +
+                        '<img class="card-img-top" alt="service_img" src="'+ img_circle_2 +'" height="250" style="padding-top: 20px">' +
+                        '<div class="card-body">' +
+                            '<h4 class="card-title">'+ service.title + '</h4>' +
+                            '<a href="service.html?id='+ service.service_id +'" class="btn btn-outline-primary" role="button"><span style="font-size: 14px"><b>FIND OUT MORE</b></span></a>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>' +
+           '</div>' +
         '</div>';
 }
 
-function createRelationContainer() {
-    return `<div class="container" id="relation_container" style="text-align: center">
-    <div class="row justify-content-center" id="relation_row">`;
-}
+function displayEventPeople(person, img_circle) {
 
-
-function displayEventPeople(person) {
-    return '<div class="col-lg-4 mb-4">'+
-        '<h2 class="custom_heading align-center">Organiser</h2>' +
-        '<div class="card h-100 text-center" style="border-radius: 15px>"> ' +
-        '<img class="card-img-top" alt="person_img" src='+ personRounded(person.img) + ' '+ 'height="250" style="padding-top: 20px">' +
-        '<div class="card-body">' +
-        '<h4 class="card-title">'+ person.name + ' '+ person.surname + '</h4>' +
-        '<p class="card-text">' + person.role + '</p>' +
-        '<a href="person.html?id='+ person.matricola +'" class="btn btn-outline-primary" role="button"><span style="font-size: 14px"><b>SEE MORE</b></span></a>' +
-        '</div>' +
-        '</div>' +
-        '</div>'
-
-}
-
-function displayEventService(service) {
-    return '<div class="col-lg-4 mb-4">'+
-        '<h2 class="custom_heading align-center">Service</h2>' +
-        '<div class="card h-100 text-center" style="border-radius: 15px>"> ' +
-        '<img class="card-img-top" alt="Missing" src='+ serviceRounded(service.img[0]) + ' '+ 'height="250" style="padding-top: 20px">' +
-        '<div class="card-body">' +
-        '<h4 class="card-title">'+ service.title  + '</h4>' +
-        '<a href="service.html?id='+ service.service_id +'" class="btn btn-outline-primary" role="button"><span style="font-size: 14px"><b>FIND OUT MORE</b></span></a>' +
-        '</div>' +
-        '</div>' +
-        '</div>';
+    return '<div class="container" style="text-align: center">' +
+        '<h2 class="custom_heading align-center" style="text-align: center">Organiser</h2>' +
+            '<div class="row justify-content-center" style="margin-left: 0">' +
+                '<div class="col-lg-4 mb-4">'+
+                        '<div class="card h-100 text-center" style="border-radius: 15px">' +
+                            '<img class="card-img-top" alt="person_img" src="'+img_circle+'" height="250" style="padding-top: 20px">' +
+                                '<div class="card-body">' +
+                                '<h4 class="card-title">'+ person.name + ' '+ person.surname + '</h4>' +
+                                '<p class="card-text">' + person.role + '</p>' +
+                                '<a href="person.html?id='+ person.matricola +'" class="btn btn-outline-primary" role="button"><span style="font-size: 14px"><b>SEE MORE</b></span></a>' +
+                                '</div>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>' +
+            '</div>';
 }
 
 function displayNavigationItems(event) {
